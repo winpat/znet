25a391eafd9884ff139da32e45741e50e3d0acce
Patrick Winter <patrickwinter@posteo.ch>
2024-06-09 17:55:42 +0200

    Train iris dataset only on training set and evaluate it on test set

--- src/main.zig
+++ src/main.zig
@@ -5,17 +5,22 @@ const Matrix = @import("matrix.zig").Matrix;
 const minMaxNormalize = @import("scale.zig").minMaxNormalize;
 const accuracy = @import("score.zig").accuracy;
 const ops = @import("ops.zig");
+const trainTestSplit = @import("split.zig").trainTestSplit;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    var features, const labels = try iris.load(
-        allocator,
-        "data/iris.csv",
-    );
-
+    var features, var labels = try iris.load(allocator, "data/iris.csv");
     minMaxNormalize(f32, &features);
+
+    const train_features, const train_labels, const test_features, const test_labels = trainTestSplit(
+        f32,
+        &features,
+        &labels,
+        0.8,
+        0.2,
+    );
 
     var net = Network(f32).init(allocator, 4, 3);
     defer net.deinit();
@@ -25,12 +30,17 @@ pub fn main() !void {
     try net.addLinear(3);
     try net.addSoftmax();
 
-    try net.train(300, 0.01, features, labels);
+    try net.train(300, 0.01, train_features, train_labels);
 
-    var predictions = try Matrix(f32).alloc(allocator, features.rows, labels.columns, .zeros);
+    var predictions = try Matrix(f32).alloc(
+        allocator,
+        test_features.rows,
+        test_labels.columns,
+        .zeros,
+    );
 
-    for (0..features.rows) |r| {
-        const X = features.getRow(r);
+    for (0..test_features.rows) |r| {
+        const X = test_features.getRow(r);
         const p = net.predict(X);
 
         // Encode predictions so that the most likely class gets mapped to 0 and
@@ -39,7 +49,7 @@ pub fn main() !void {
         predictions.set(r, c, 1.0);
     }
 
-    const acc = accuracy(f32, predictions, labels);
+    const acc = accuracy(f32, predictions, test_labels);
     std.debug.print("Accuracy: {d:.3}\n", .{acc});
 }
 
