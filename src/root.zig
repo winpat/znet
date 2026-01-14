c0b95c8a4c8500b82cccb55d709c72ff12691c6d
Patrick Winter <patrickwinter@posteo.ch>
2026-01-14 17:43:09 +0100

    Add missing source files to root.zig for test discovery

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,14 @@
-const iris = @import("iris.zig");
-const Network = @import("net.zig").Network;
-const Matrix = @import("matrix.zig").Matrix;
-const minMaxNormalize = @import("scale.zig").minMaxNormalize;
-const accuracy = @import("score.zig").accuracy;
-const ops = @import("ops.zig");
+pub const csv = @import("csv.zig");
+pub const iris = @import("iris.zig");
+pub const layer = @import("layer.zig");
+pub const matrix = @import("matrix.zig");
+pub const mse = @import("mse.zig");
+pub const net = @import("net.zig");
+pub const ops = @import("ops.zig");
+pub const scale = @import("scale.zig");
+pub const score = @import("score.zig");
+pub const split = @import("split.zig");
+
+test {
+    @import("std").testing.refAllDecls(@This());
+}
